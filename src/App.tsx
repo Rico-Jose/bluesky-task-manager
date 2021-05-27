67d2ff936d912a176ab31ab90d9b600d9980a58f
Rico-Jose <imbangjoserico@gmail.com>
2021-05-28 03:31:55 +0800

    set users & tasks state

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
   const retrieveUsers = () => {
     axios.get('api/users').then((res) => {
-      console.log(res.data.users);
+      setUsers([...users, ...res.data.users]);
     });
   };
 
   const retrieveTasks = () => {
     axios.get('api/todos').then((res) => {
-      console.log(res.data.todos);
+      setTasks([...tasks, ...res.data.todos]);
     });
   };
+
+  const [users, setUsers] = useState<any>([]);
+  const [tasks, setTasks] = useState<any>([]);
 
   // Run the hook on mount
   useEffect(() => {
     retrieveUsers();
     retrieveTasks();
   }, []);
+
+  // Run the hook whenever users change
+  useEffect(() => {
+    console.log(users);
+  }, [users]);
+
+  // Run the hook whenever tasks change
+  useEffect(() => {
+    console.log(tasks);
+  }, [tasks]);
 
   return (
     <div className="App">
