3cc96dcced21d87dbc80597723bcf98f20a170cb
Rico-Jose <imbangjoserico@gmail.com>
2021-05-28 13:41:13 +0800

    create TaskDetail component

---
+++ src/TaskDetail.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TaskDetail = () => {
+  return (
+    <main>
+      <div className="card">
+        <div className="card-body">
+          <h1 className="card-title">Do something</h1>
+          <p className="card-text">Rico</p>
+        </div>
+      </div>
+      <div>
+        <button className="btn btn-primary">Back to Task List</button>
+      </div>
+    </main>
+  );
+};
+
+export default TaskDetail;
