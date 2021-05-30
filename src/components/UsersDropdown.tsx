f0d031bc33ca6fb61185146442874f95d680d868
Rico-Jose <imbangjoserico@gmail.com>
2021-05-31 01:58:43 +0800

    implement Material-UI into UsersDropdown

--- src/components/UsersDropdown.tsx
+++ src/components/UsersDropdown.tsx
@@ -1,4 +1,53 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  })
+);
+
+export default function UsersDropdown(props: any) {
+  const classes = useStyles();
+  const [user, setUser] = React.useState('');
+
+  const handleChange = (event: React.ChangeEvent<{ value: any }>) => {
+    setUser(event.target.value as string);
+    props.getUserId(event.target.value as string);
+  };
+
+  return (
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">User</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={user}
+          onChange={handleChange}
+        >
+          {props.users.map((user: any) => (
+            <MenuItem value={user.id} key={user.id}>
+              {user.firstName}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+/* import React, { useState } from 'react';
 
 const UsersDropdown = (props: any) => {
   const [open, setOpen] = useState(false);
@@ -45,3 +94,4 @@ const UsersDropdown = (props: any) => {
 };
 
 export default UsersDropdown;
+ */
