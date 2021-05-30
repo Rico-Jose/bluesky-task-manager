c5164d2d8a0a63fb8841951994bd481031b6822e
Rico-Jose <imbangjoserico@gmail.com>
2021-05-31 02:14:15 +0800

    change UsersDropdown layout

--- src/components/UsersDropdown.tsx
+++ src/components/UsersDropdown.tsx
@@ -8,8 +8,7 @@ import Select from '@material-ui/core/Select';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     formControl: {
-      margin: theme.spacing(1),
-      minWidth: 120,
+      minWidth: 340,
     },
     selectEmpty: {
       marginTop: theme.spacing(2),
@@ -35,6 +34,7 @@ export default function UsersDropdown(props: any) {
           id="demo-simple-select"
           value={user}
           onChange={handleChange}
+          required
         >
           {props.users.map((user: any) => (
             <MenuItem value={user.id} key={user.id}>
@@ -46,52 +46,3 @@ export default function UsersDropdown(props: any) {
     </div>
   );
 }
-
-/* import React, { useState } from 'react';
-
-const UsersDropdown = (props: any) => {
-  const [open, setOpen] = useState(false);
-
-  const toggle = (e: boolean) => setOpen(!open);
-
-  return (
-    <div className="dropdown">
-      <button
-        className="btn btn-secondary dropdown-toggle"
-        type="button"
-        id="dropdownMenuButton"
-        data-toggle="dropdown"
-        aria-haspopup="true"
-        aria-expanded="false"
-        onKeyPress={() => toggle(!open)}
-        onClick={() => toggle(!open)}
-        style={{
-          display: 'block',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-        }}
-      >
-        Select User
-      </button>
-      {open && (
-        <div>
-          {props.users.map((user: any) => (
-            <a
-              className="dropdown-item"
-              key={user.id}
-              onClick={() => {
-                toggle(!open);
-                props.getUserId(user.id);
-              }}
-            >
-              {user.firstName}
-            </a>
-          ))}
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default UsersDropdown;
- */
