c0841feec4dcb8323eb48b2bb5b051ddbda88053
Rico-Jose <imbangjoserico@gmail.com>
2021-06-03 09:51:02 +0800

    create TaskTable component

--- src/tests/App.tsx
+++ src/tests/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { TaskProvider } from '../contexts/TaskContext';
 import Header from './Header';
+import TaskTable from './TaskTable';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
-    <TaskProvider>
-      <Header />
-      <h2>React</h2>
-    </TaskProvider>
+    <div className="App">
+      <TaskProvider>
+        <Header />
+        <div className="container">
+          <main>
+            <TaskTable />
+          </main>
+        </div>
+      </TaskProvider>
+    </div>
   );
 }
